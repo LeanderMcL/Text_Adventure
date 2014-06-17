fd32d6ab17b52be2610de055ce4b2847db857379
Lucy <lucymclaughlin.code@gmx.com>
2014-06-17 01:16:16 +0100

    Added three rooms to the game. Also modified .gitignore.

--- rooms.py
+++ rooms.py
@@ -24,3 +24,9 @@ previous_room = ""
 courtyard = Room("courtyard","A courtyard with ivy hanging from all the walls.","You find yourself in a cool courtyard, lined with buildings on all sides. Ivy covers each of the four walls, leaving only small gaps for the doors leading into the buildings. The floor is paved with stone slabs.",{"north": "iron door", "east": "glass door", "south": "wooden door", "west": "blue door"})
 
 conservatory = Room("conservatory","A glass-domed conservatory.","You are in a glass-domed conservatory. This room is oddly bare, but for a couple of benches up against the back wall.",{"west": "glass door"})
+
+ballroom = Room("ballroom","An empty, echoing ballroom.","This ballroom is huge. And very, very dusty: no one has been here in a long time. A chandelier, hung with cobwebs, hangs above the centre of the room. The furniture is very spare: a few chairs and small tables are shoved up against the walls, leaving a vast, empty dancefloor. A grand piano stands against one wall, also dusty and unused.",{"south":"iron door"})
+
+kitchen = Room("kitchen","A dusty kitchen.","This kitchen was probably once cosy and warm. Now, all the counter-tops are covered in a thick layer of dust. So is the wooden table which stands in the centre of the room.",{"north":"wooden door"})
+
+gardeners_alcove = Room("gardener's alcove","A gardener's alcove.","This alcove is little more than a hole in the western wall of the courtyard. A table, shoved against the back wall, is stacked with tools and gardening equipment. There is dust everywhere, and cobwebs hang from the corners of the tiny room.",{"east":"blue door"})
